f95754b084778fa9359a21486122f523b1a1bccf
StarostinaVS <starostinavs@zabgu.ru>
2024-09-16 16:21:49 +0900

    improvements + creation of classes for the main page, product page, goods page and authorization

--- main/views.py
+++ main/views.py
@@ -1,20 +1,42 @@
+from typing import Any
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from goods.models import Categories
 
-def index(request):
+class IndexView(TemplateView):
+    template_name='main/index.html'
 
-    context = {
-        "title": "Home - Главная",
-        "content": "Магазин мебели HOME",
-    }
-    return render(request, 'main/index.html', context)
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context=super().get_context_data(**kwargs)
+        context["title"] = "Home - Главная"
+        context["content"] = "Магазин мебели HOME"
+        return context
 
 
-def about(request):
-    context = {
-        "title": "Home - О нас", 
-        "content": "О нас",
-        "text_on_page": "Текст о нас",
-        }
-    return render(request, 'main/about.html', context)
+class AboutView(TemplateView):
+    template_name = "main/about.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home - О нас"
+        context["content"] = "О нас"
+        context["text_on_page"] = "Текст о нас"
+        return context
+
+# def index(request):
+
+#     context = {
+#         "title": "Home - Главная",
+#         "content": "Магазин мебели HOME",
+#     }
+#     return render(request, 'main/index.html', context)
+
+
+# def about(request):
+#     context = {
+#         "title": "Home - О нас",
+#         "content": "О нас",
+#         "text_on_page": "Текст о нас",
+#         }
+#     return render(request, 'main/about.html', context)
--- users/views.py
+++ users/views.py
@@ -1,42 +1,46 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from carts.models import Cart
+from orders.models import Order, OrderItem
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
-def login(request):
-    if request.method == "POST":
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST["username"]
-            password = request.POST["password"]
-            user = auth.authenticate(username=username, password=password)
+class UserLoginViews(LoginView):
+    template_name = "login.html"
+    form_class = UserLoginForm
+    # success_url = reverse_lazy ("main:index")
 
-            session_key = request.session.session_key
+    def get_success_url(self) -> str:
+        redirect_page = self.request.POST.get("next", None)
+        if redirect_page and redirect_page != reverse_lazy("user:login"):
+            return redirect_page
+        return reverse_lazy("main:index")
 
-            if user:
-                auth.login(request, user)
-                messages.success(request, f"{username}, вы вошли в аккаунт")
+    def form_valid(self, form):
+        session_key = self.request.session.session_key
+        user = form.get_user()
 
-                if session_key:
-                    Cart.objects.filter(session_key=session_key).update(user=user)
+        if user:
+            auth.login(self.request, user)
+            if session_key:
+                forgot_carts = Cart.objects.filter(user=user)
+                if forgot_carts.exists():  # если старая корзина не пуста
+                    forgot_carts.delete()  # то удаляем ее
+                Cart.objects.filter(session_key=session_key).update(user=user)
+                messages.success(self.request, f"{user.username}, вы вошли в аккаунт")
 
-                redirect_page = request.POST.get("next", None)
-                if redirect_page and redirect_page != reverse('user:login'):
-                    return HttpResponseRedirect(request.POST.get("next"))
-                return HttpResponseRedirect(reverse("main:index"))
-    else:
-        form = UserLoginForm()
+                return HttpResponseRedirect(self.get_success_url())
 
-    context = {
-        "title": "Home - Авторизация",
-        "form": form,
-    }
-    return render(request, "login.html", context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Авторизация"
+        return context
 
 
 def registration(request):
@@ -46,7 +50,7 @@ def registration(request):
             form.save()
 
             session_key = request.session.session_key
-            
+
             user = form.instance
             auth.login(request, user)
 
@@ -78,7 +82,18 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
 
-    context = {"title": "Home - Кабинет", "form": form}
+    orders = (
+        Order.objects.filter(user=request.user)
+        .prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        )
+        .order_by("-id")
+    )
+
+    context = {"title": "Home - Кабинет", "form": form, "orders": orders}
     return render(request, "profile.html", context)
 
 
@@ -91,3 +106,34 @@ def logout(request):
     messages.success(request, f"{request.user.username}, вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse("main:index"))
+
+
+# def login(request):
+#     if request.method == "POST":
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST["username"]
+#             password = request.POST["password"]
+#             user = auth.authenticate(username=username, password=password)
+
+#             session_key = request.session.session_key
+
+#             if user:
+#                 auth.login(request, user)
+#                 messages.success(request, f"{username}, вы вошли в аккаунт")
+
+#                 if session_key:
+#                     Cart.objects.filter(session_key=session_key).update(user=user)
+
+#                 redirect_page = request.POST.get("next", None)
+#                 if redirect_page and redirect_page != reverse("user:login"):
+#                     return HttpResponseRedirect(request.POST.get("next"))
+#                 return HttpResponseRedirect(reverse("main:index"))
+#     else:
+#         form = UserLoginForm()
+
+#     context = {
+#         "title": "Home - Авторизация",
+#         "form": form,
+#     }
+#     return render(request, "login.html", context)
